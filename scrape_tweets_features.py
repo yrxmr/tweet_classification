8288d605b1c3029ed4ccd539212b85c16e819fa7
Yara A <59065688+yrxmr@users.noreply.github.com>
2024-06-06 14:42:09 +0200

    Update scrape_tweets_features.py

--- scrape_tweets_features.py
+++ scrape_tweets_features.py
@@ -3,212 +3,98 @@ import pandas as pd
 from itertools import groupby
 import re
 
+# Utilisation du modèle Twint pour effectuer du scraping
+def scrape(keywords, since, outfile):
+    c = twint.Config()
+    c.Search = keywords  # search keyword
+    c.Since = since
+    c.Store_csv = True
+    c.Output = "./" + outfile
+    c.Hide_output = True
+    c.Count = True
+    c.Stats = True
+    c.Resume = 'resume.txt'
+    twint.run.Search(c)
 
-#utilisation du modèle Twint pour effectuer du scraping
+def get_tweets(infile, outfile):
+    df = pd.read_csv(infile, usecols=["tweet"])
+    df["tweet"].to_csv(outfile, index=False, header=False)
 
-def scrape (keywords, since, outfile):
-        c = twint.Config()
-        c.Search = keywords #search keyword
-        c.Since = since
-        c.Store_csv = True
-        c.Output = "./" + outfile
-        c.Hide_output = True
-        c.Count = True
-        c.Stats = True
-        c.Resume = 'resume.txt'
-        twint.run.Search(c)
+# Features
+# Tous les features sont calculés au niveau de la phrase, donc au niveau d'un tweet
+# Ils sont ensuite concaténés pour faire un dataframe comprenant l'ensemble des features
 
+def parse_conllu(conllu_file):
+    with open(conllu_file, "r") as file:
+        lines = [re.sub("!|\?|\.{1,3}|,|:", "", line) for line in file]
+    return [list(g) for k, g in groupby(lines, key=lambda x: x != '\n') if k]
 
-def get_tweets(infile,outfile):
-        col_list = ["tweet"]
-        df = pd.read_csv(infile, usecols=col_list)
-        rows = df["tweet"]
-        with open(outfile, "w+") as f:
-                for row in rows:
-                        f.write(row+"\n")
-                
+def compute_features(sentences):
+    percent_noun, percent_adj, percent_verb, lengths = [], [], [], []
+    percent_inf, percent_plur, percent_sing = [], [], []
+    percent_1st, percent_2nd, percent_3rd = [], [], []
+    percent_punct, avg_word_len = [], []
 
-#features
-#tous les features sont calculés au niveau de la phrase, donc au niveau d'un tweet
-#ils sont ensuite concaténés pour faire un dataframe comprenant l'ensemble des features
-                        
-def conllulemme(conllu):
-    global dfs
-    global df1
-    global df2
-    global df3
-    global df4
-    global lines
-    dfs = []
-    with open(conllu, "r") as file:
-        lines = []
-        for line in file:
-            lines.append(line)
-    for line in lines:
-            replacement = re.sub("!|\?|\.{1,3}|,|:", "", line)
-            line = replacement
-            
-    sentences = (list(g) for k,g in groupby(lines, key=lambda x: x != '\n') if k)
-    percentagenoun = []
-    percentageadj = []
-    percentagev = []
-    leng = []
     for sentence in sentences:
-        x = len(sentence)
-        a = 0
-        b = 0
-        c = 0
-        for item in sentence:
-            if "NOUN" in item or "PROPN" in item:
-                a+=1
-            if "ADJ" in item:
-                b+=1
-            if "VERB" in item:
-                c+=1
-        if a > 0:
-            a = (a/x)
-        else:
-            a = "0"
-        if b > 0:
-            b = (b/x)
-        else:
-            b = "0"
-        if c > 0:
-            c = (c/x)
-        else:
-            c = "0"
-        percentagenoun.append(a)
-        percentageadj.append(b)
-        percentagev.append(c)
-        leng.append(x)
-        df1 = pd.DataFrame(percentagenoun, columns=['NOM%'])
-        df2 = pd.DataFrame(percentageadj, columns=['ADJ%'])
-        df3 = pd.DataFrame(percentageadj, columns=['VERB%'])
-        df4 = pd.DataFrame(leng, columns=['NB LEMMES'])
-        dfs.extend([df1, df2, df3, df4])
+        length = len(sentence)
+        lengths.append(length)
 
-
+        noun_count = sum(1 for item in sentence if "NOUN" in item or "PROPN" in item)
+        adj_count = sum(1 for item in sentence if "ADJ" in item)
+        verb_count = sum(1 for item in sentence if "VERB" in item)
         
-def conlluverbes():
-    global df5
-    global df6
-    global df7
-    global df8
-    global df9
-    global df10
-    sentences = (list(g) for k,g in groupby(lines, key=lambda x: x != '\n') if k)
-    percentageinf = []
-    percentageplur = []
-    percentagesing = []
-    firstpers = []
-    secondpers = []
-    thirdpers = []
-    for sentence in sentences:
-        a = 0
-        b = 0
-        c = 0
-        d = 0
-        e = 0
-        f = 0
-        allv = 0
-        for item in sentence:
-            if "VerbForm=Inf" in item:
-                    a+= 1
-            if "VerbForm" in item:
-                    allv+= 1
-            if "VERB" in item and "Number=Plur" in item:
-                    b+= 1
-            if "VERB" in item and "Number=Sing" in item:
-                    c+= 1
-            if "Person=1" in item and "VERB" in item:
-                    d+=1
-            if "Person=2" in item and "VERB" in item:
-                    e+=1
-            if "Person=3" in item and "VERB" in item:
-                    f+=1
-                    
-        if a > 0 and allv > 0:
-            a = (a/allv)
-            percentageinf.append(a)
-        else:
-            percentageinf.append("0")
-        if b > 0 and allv > 0:
-            b = (b/allv)
-            percentageplur.append(b)
-        else:
-            percentageplur.append("0")
-        if  c > 0 and allv > 0:
-                c = (c/allv)
-                percentagesing.append(c)
-        else:
-            percentagesing.append("0")
-        if  d > 0 and allv > 0:
-                d = (d/allv)
-                firstpers.append(d)
-        else:
-            firstpers.append("0")
-        if  e > 0 and allv > 0:
-                e = (e/allv)
-                secondpers.append(e)
-        else:
-            secondpers.append("0")
-        if  f > 0 and allv > 0:
-                f = (f/allv)
-                thirdpers.append(e)
-        else:
-            thirdpers.append("0")
-        df5 = pd.DataFrame(percentageinf, columns=['VINF%'])
-        df6 = pd.DataFrame(percentageplur, columns=['VPLUR%'])
-        df7 = pd.DataFrame(percentagesing, columns=['VSING%'])
-        df8 = pd.DataFrame(firstpers, columns=['1ère PER%'])
-        df9 = pd.DataFrame(secondpers, columns=['2ème PER%'])
-        df10 = pd.DataFrame(thirdpers, columns=['3ème PER%'])
-        dfs.extend([df5, df6, df7, df8, df9, df10])
+        percent_noun.append(noun_count / length if length else 0)
+        percent_adj.append(adj_count / length if length else 0)
+        percent_verb.append(verb_count / length if length else 0)
+
+        inf_count = sum(1 for item in sentence if "VerbForm=Inf" in item)
+        verb_total = sum(1 for item in sentence if "VerbForm" in item)
+        plur_count = sum(1 for item in sentence if "VERB" in item and "Number=Plur" in item)
+        sing_count = sum(1 for item in sentence if "VERB" in item and "Number=Sing" in item)
+        first_pers_count = sum(1 for item in sentence if "Person=1" in item and "VERB" in item)
+        second_pers_count = sum(1 for item in sentence if "Person=2" in item and "VERB" in item)
+        third_pers_count = sum(1 for item in sentence if "Person=3" in item and "VERB" in item)
+
+        percent_inf.append(inf_count / verb_total if verb_total else 0)
+        percent_plur.append(plur_count / verb_total if verb_total else 0)
+        percent_sing.append(sing_count / verb_total if verb_total else 0)
+        percent_1st.append(first_pers_count / verb_total if verb_total else 0)
+        percent_2nd.append(second_pers_count / verb_total if verb_total else 0)
+        percent_3rd.append(third_pers_count / verb_total if verb_total else 0)
+
+        punct_count = sum(1 for item in sentence if "punct" in item)
+        percent_punct.append(punct_count / length if length else 0)
         
-        
-def conllugraphie():
-    global df11
-    global df12
-    global lines
-    sentences = (list(g) for k,g in groupby(lines, key=lambda x: x != '\n') if k)
-    a = 0
-    b = 0
-    percentagepunct= []
-    length = []
-    lenword = []
-    for sentence in sentences:
-        x = len(sentence)
-        length.append(x)
-        for item in sentence:
-                if "punct" in item:
-                    a+=1
-                if a > 0 and x > 0:
-                        a = (a/x)
-                        percentagepunct.append(a)
-                word = re.findall(r"\w+", item)
-                if len(word) > 1 and word != "PUNCT":
-                        b+=(len(word[1]))
-        b = (b/x)
-        lenword.append(b)
+        word_lengths = [len(word) for item in sentence for word in re.findall(r"\w+", item) if word != "PUNCT"]
+        avg_word_len.append(sum(word_lengths) / len(word_lengths) if word_lengths else 0)
 
+    data = {
+        'NOM%': percent_noun,
+        'ADJ%': percent_adj,
+        'VERB%': percent_verb,
+        'NB LEMMES': lengths,
+        'VINF%': percent_inf,
+        'VPLUR%': percent_plur,
+        'VSING%': percent_sing,
+        '1ère PER%': percent_1st,
+        '2ème PER%': percent_2nd,
+        '3ème PER%': percent_3rd,
+        'PUNCT%': percent_punct,
+        'AVG LEN WORD': avg_word_len
+    }
 
-                        
-                
-                        
-                        
-    df11 = pd.DataFrame(percentagepunct, columns=['PUNCT%'])
-    df12 = pd.DataFrame(lenword, columns=['AVG LEN WORD'])
-    dfs.extend([df11, df12])
+    return pd.DataFrame(data)
 
-    
+def save_to_csv(df, file):
+    df.to_csv(file, index=False)
 
-def csv(file):
-        result = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12], axis=1).reindex(df1.index)
-        result.to_csv(file)
-   
-#scrape('couvre-feu', '2021-01-1 15:55:00', 'AAtweets.csv')
-#get_tweets("AAtweets.csv","outfile.txt")
-conllulemme(fichierconllu)
-conlluverbes()
-conllugraphie()
-csv(fichiercsv)
+# Usage
+# scrape('couvre-feu', '2021-01-1 15:55:00', 'AAtweets.csv')
+# get_tweets("AAtweets.csv","outfile.txt")
+
+conllu_file = 'conllu/file'  
+output_csv = 'output.csv'  
+
+sentences = parse_conllu(conllu_file)
+features_df = compute_features(sentences)
+save_to_csv(features_df, output_csv)
